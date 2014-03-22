7f67d69ac5aa82a04bc7a617ddee9cc429619406
Nicolas Alliaume <nicolasalliaume@gmail.com>
2014-03-22 13:36:34 -0300

    import new ones

---
+++ Module_2/exercises/04-lists-index/exercise.py
@@ -0,0 +1,17 @@
+#############################################
+#
+# Each element of a list has an index so it can
+# be referenced. 
+#
+# The first element of a list is at index 0, and
+# it can be obstained using the_list[0]
+#
+# Print the elements at positions 1, 4 and 6.
+#
+#############################################
+
+the_list = ['some', 'nonsense', 'elements', 'of', 'a', 'list', 'are', 'written', 'in', 'here']
+
+print (  )
+print (  )
+print (  )
---
+++ Module_2/exercises/05-sublists/exercise.py
@@ -0,0 +1,17 @@
+#############################################
+#
+# It's really easy to get a portion of a list.
+# You can use the_list[<from>:<to>], where <from>
+# is one index and <to> is another index.
+#
+# Try to print from index 2 to 5 of the given
+# list.
+#
+# Tip: using print() with a list as a parameter will
+# do fine.
+#
+#############################################
+
+the_list = ['some', 'nonsense', 'elements', 'of', 'a', 'list', 'are', 'written', 'in', 'here']
+
+print (  )
