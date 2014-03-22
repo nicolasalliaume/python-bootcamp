784fe2050ebb16e3502c337fe90990eea0fccc6c
Nicolas Alliaume <nicolasalliaume@gmail.com>
2014-03-22 13:23:12 -0300

    Fixed

--- Module_2/exercises/02-list-of-lists/exercise.py
+++ Module_2/exercises/02-list-of-lists/exercise.py
@@ -1,1 +1,12 @@
-e
+#############################################
+#
+# Print on the console each item of the lists
+# inside 'the_big_list'
+#
+#############################################
+
+the_big_list = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+
+for :
+	for :
+		print (  )
